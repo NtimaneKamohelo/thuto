ff651f41821ecc1495636e2be897b09e9e7735ea
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2025-10-01 16:16:50 +0200

    Created the UserController, ResourceNotFoundException, files...

--- Thuto/src/main/java/com/kamohelontimane/Thuto/service/impl/UserServiceImpl.java
+++ Thuto/src/main/java/com/kamohelontimane/Thuto/service/impl/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package com.kamohelontimane.Thuto.service.impl;
 
 import com.kamohelontimane.Thuto.entity.User;
+import com.kamohelontimane.Thuto.exception.ResourceNotFoundException;
 import com.kamohelontimane.Thuto.repository.UserRepository;
 import com.kamohelontimane.Thuto.service.UserService;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -33,6 +34,27 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User updateUser(Long id, User updated) {
+        return userRepository.findById(id).map(existing -> {
+            existing.setName(updated.getName());
+            existing.setBio(updated.getBio());
+            existing.setRole(updated.getRole());
+            return userRepository.save(existing);
+        }).orElseThrow(() -> new ResourceNotFoundException("User not found with id " + id));
+    }
+
+    @Override
+    public void deleteUser(Long id){
+        if (!userRepository.existsById(id)) {
+            throw new ResourceNotFoundException("User not fount with id " + id);
+        }
+        userRepository.deleteById(id);
+    }
 
 
 }
