801102ff3cb0806ff54c09e040dbd42fee095be9
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2025-10-01 17:30:02 +0200

    Remove applicatoin.properties from repository

--- Thuto/src/main/java/com/kamohelontimane/Thuto/controller/UserController.java
+++ Thuto/src/main/java/com/kamohelontimane/Thuto/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.kamohelontimane.Thuto.controller;
 
+import com.kamohelontimane.Thuto.entity.User;
 import com.kamohelontimane.Thuto.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -14,5 +19,32 @@ public class UserController {
         this.userService = userService;
     }
 
-    
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User saved = userService.createUser(user);
+        return ResponseEntity.created(URI.create("/api/users/" + saved.getId())).body(saved);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userService.getUserById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User payload){
+        User updated = userService.updateUser(id, payload);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
