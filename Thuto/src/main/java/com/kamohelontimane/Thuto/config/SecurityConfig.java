a7b22f8bd9c063c7bcc9827bb1ba5dae128bdccc
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2025-10-07 10:57:22 +0200

    added authenticationManager method under SecurityConfig to satisfy AuthController

--- Thuto/src/main/java/com/kamohelontimane/Thuto/config/SecurityConfig.java
+++ Thuto/src/main/java/com/kamohelontimane/Thuto/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.kamohelontimane.Thuto.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +18,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
     }
 
     //Security filter chain
