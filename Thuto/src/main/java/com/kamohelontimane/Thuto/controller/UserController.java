ec18139f46fbc1906252481c291463908b15d878
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2025-10-13 15:33:39 +0200

    Updated the user controller

--- Thuto/src/main/java/com/kamohelontimane/Thuto/controller/UserController.java
+++ Thuto/src/main/java/com/kamohelontimane/Thuto/controller/UserController.java
@@ -4,9 +4,13 @@ import com.kamohelontimane.Thuto.entity.User;
 import com.kamohelontimane.Thuto.service.UserService;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.security.Security;
 import java.util.List;
 
 @RestController
@@ -19,33 +23,19 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user){
-        User saved = userService.createUser(user);
-        return ResponseEntity.created(
-                URI.create("/api/users/" + saved.getId())).body(saved);
-    }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
-        return userService.getUserById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    @GetMapping("/me")
+    public ResponseEntity<User> authenticatedUser(){
+        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(currentUser);
     }
 
     @GetMapping
     public ResponseEntity<List<User>> getAll(){
-        return ResponseEntity.ok(userService.getAllUsers());
+        List<User> users = userService.allUsers();
+        return ResponseEntity.ok(users);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User payload){
-        User updated = userService.updateUser(id, payload);
-        return ResponseEntity.ok(updated);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
-        userService.deleteUser(id);
-        return ResponseEntity.noContent().build();
-    }
 
 }
--- Thuto/src/main/java/com/kamohelontimane/Thuto/service/UserService.java
+++ Thuto/src/main/java/com/kamohelontimane/Thuto/service/UserService.java
@@ -1,12 +1,26 @@
 package com.kamohelontimane.Thuto.service;
 
 import com.kamohelontimane.Thuto.entity.User;
+import com.kamohelontimane.Thuto.repository.UserRepository;
+import com.kamohelontimane.Thuto.service.impl.EmailService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserService {
+public class UserService {
 
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository, EmailService emailService){
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers(){
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
 }
